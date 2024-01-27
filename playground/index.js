36d5b660d052272945093e9a571b4c8aa941fad2
kanido386 <kanido386@gmail.com>
2024-01-27 23:50:29 +0800

    try: Can input email with puppeteer

--- playground/index.js
+++ playground/index.js
@@ -1,4 +1,13 @@
-const puppeteer = require('puppeteer')
+require('dotenv').config()
+// const puppeteer = require('puppeteer')
+
+// puppeteer-extra is a drop-in replacement for puppeteer,
+// it augments the installed puppeteer with plugin functionality
+const puppeteer = require('puppeteer-extra')
+
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 async function main() {
   // Launch the browser and open a new blank page
@@ -6,13 +15,23 @@ async function main() {
   const page = await browser.newPage()
 
   // Navigate the page to a URL
-  await page.goto('https://feedly.com/homepage')
+  await page.goto(process.env.LOGIN_URL, { waitUntil: 'networkidle2' })
 
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 })
+  await page.waitForSelector('input[type=email]')
+  await page.type('input[type=email]', process.env.EMAIL)
+  await page.keyboard.press('Enter')
 
-  await page.screenshot({ path: 'test.png' })
-  await browser.close()
+  // TODO: Can't work:
+  // await page.waitForNavigation({ waitUntil: 'networkidle2' })
+  // await page.waitForSelector('input[type=password]')
+  // await page.type('input[type=password]', process.env.PASSWORD)
+  // await page.keyboard.press('Enter')
+
+  // // Set screen size
+  // await page.setViewport({ width: 1080, height: 1024 })
+
+  // await page.screenshot({ path: 'test.png' })
+  // await browser.close()
 }
 
 main().catch(err => console.log(err))
